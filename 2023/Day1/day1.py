bc924988d6331ce56eb23aec1c7f0bc684e86ee5
Hyan <hyanxjan@gmail.com>
2024-08-01 02:00:04 -0400

    Completed the challenge. Sum of each key was added and prints the value

--- 2023/Day1/day1.py
+++ 2023/Day1/day1.py
@@ -1,27 +1,31 @@
-
+# Declare initial input for while loop to check
 puzzleArray = []
-puzzleKey = input("Enter puzzle keys: \n")
+puzzleKey = input("Enter puzzle keys: \n")              
 
+# Allows infinite input until exit is given
 while puzzleKey != 'exit':
     puzzleArray.append(puzzleKey)
     puzzleKey = input()
 
 puzzleCode = []
 
+# Loops through each key and takes the first and last integer
 for x in range(len(puzzleArray)):
-    firstInt = '0'
-    secondInt = '0'
+    firstInt = None
+    secondInt = None
+    intCheck = False
 
-    for count, character in enumerate(puzzleArray[x]):
-        intCheck = False
+    for count, character in enumerate(puzzleArray[x]):     
         if character.isdigit() == True and intCheck == False:
             firstInt = character
             intCheck = True
         elif character.isdigit() == True:
             secondInt = character
+    
+    if secondInt == None:
+        secondInt = firstInt
 
     puzzleCode.append(int(firstInt + secondInt))
 
+print("{}{}".format("The answer is ", sum(puzzleCode)))
 
-for x in range(len(puzzleCode)):
-    print(puzzleCode[x])

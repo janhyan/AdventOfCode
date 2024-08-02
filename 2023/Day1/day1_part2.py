0b31350bb8bca13a795ffadb129957ccc71f034b
Hyan <hyanxjan@gmail.com>
2024-08-02 12:24:59 -0400

    Completed Day 1. Part 2 solved through regex

--- 2023/Day1/day1_part2.py
+++ 2023/Day1/day1_part2.py
@@ -11,13 +11,27 @@ while puzzleKey != 'exit':
     puzzleArray.append(puzzleKey)
     puzzleKey = input()
 
+# Convert number words to integer
+
+def word2int(word):
+    ones = {
+        "one": '1', "two": '2', "three": '3',
+        "four": '4', "five": '5', "six": '6',
+        "seven": '7', "eight": '8', "nine": '9'
+    }
+
+    if word in ones:
+        return ones[word]
+    else:
+        return word
+
 puzzleCode = []
 
 # Loops through each key and takes the first and last integer
 for x in range(len(puzzleArray)):
-    # patterns = ['one', 'two', 'three',
-    #             'four', 'five', 'six',
-    #             'seven', 'eight', 'nine']
-    keys = re.findall(r"\Bone\B|\Btwo\B|\Bthree\B|\Bfour\B|\Bfive\B|\Bsix\B|\Bseven\B|\Beight\B|\Bnine\B|", puzzleArray[x])
-    print(keys[x])
+    keys = re.findall(r"(?=(one|two|three|four|five|six|seven|eight|nine|\d))", puzzleArray[x])
+
+    puzzleCode.append(int(word2int(keys[0]) + word2int(keys[len(keys)-1])))
+  
+print("{}{}".format("The answer is ", sum(puzzleCode)))
 

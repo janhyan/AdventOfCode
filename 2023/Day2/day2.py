806548ad9d2c763dc87d86b530532c8ea1eef48f
Hyan <hyanxjan@gmail.com>
2024-08-04 13:16:37 -0400

    Completed Day 2 Part 1.

--- 2023/Day2/day2.py
+++ 2023/Day2/day2.py
@@ -3,13 +3,24 @@
 import aocd
 import re
 
-# print(aocd.get_data(day=2, year=2023))
+# Create function to get totals and adds to list
 
-def getTotalForGame():
-    gameTotals = ['1 red']
-    for x in gameTotals:
-        gameTotals = re.findall(r"\d(?=red)", '1 red')
+def getTotalForGame(input):
+    gamesList = input.splitlines()
+    gamesTotal = []
 
-    print(gameTotals)
+    # Loops through each line for red, blue, and green
+    for x in range(len(gamesList)):
+        red = re.findall(r"(\d|\d{2})(?= red)", gamesList[x])
+        blue = re.findall(r"(\d|\d{2})(?= blue)", gamesList[x])
+        green = re.findall(r"(\d|\d{2})(?= green)", gamesList[x])
+        
+        if all(int(i) <= 12 for i in red) and all(int(i) <= 14 for i in blue) and all(int(i) <= 13 for i in green): 
+            gamesTotal.append(x + 1)
 
-getTotalForGame()
+    print("The answer is", sum(gamesTotal))
+    
+
+getTotalForGame(aocd.get_data(day=2, year=2023))
+
+
